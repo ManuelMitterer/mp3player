4e10b10283f5557543869e841424f92c3696f4fa
Manuel Mitterer <mmitterer.mmt-b2011@fh-salzburg.at>
2015-03-20 16:05:49 +0100

    cover working

--- test/test_spec.js
+++ test/test_spec.js
@@ -1,8 +1,16 @@
-var assert = require("assert");
+global.window = {};
+global.webkitAudioContext = {};
+
+var assert = require("assert"),
+    mp3 = require("../js/model.js"),
+    should = require("should");
+
 
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
+      var mp3Model = new mp3.Mp3Model();
+
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
     })
