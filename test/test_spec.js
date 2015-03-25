caf2e42870152ac6a9faf3f62d7edd2dcc85e8ce
Lina Tang <ltang.mmt-b2011@fh-salzburg.ac.at>
2015-03-25 11:51:08 +0100

    fixed bug of creating model

--- test/test_spec.js
+++ test/test_spec.js
@@ -3,16 +3,21 @@ global.webkitAudioContext = {};
 
 var assert = require("assert"),
     mp3 = require("../js/model.js"),
-    should = require("should");
+    should = require("should"),
+    mp3Model,
+    timeOut;
+
+var createModel = function() {
+    return mp3Model = new mp3.Mp3Model();
+}
 
 
 describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      var mp3Model = new mp3.Mp3Model();
-
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+    describe('#indexOf()', function(){
+        it('should return -1 when the value is not present', function(){
+            model = setInterval(createModel, 300);
+            model.mute;
+           // assert(true);
+        })
     })
-  })
 })
